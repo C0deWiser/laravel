03d460e01fe977600bd6ce2141e7c82683009cea
pm <pm@101media.ru>
2025-02-05 15:12:12 +0300

    Rename HasDeletedAt Trait

--- src/Database/Eloquent/Traits/HasDeletedAt.php
+++ src/Database/Eloquent/Traits/HasDeletedAt.php
@@ -2,46 +2,10 @@
 
 namespace Codewiser\Database\Eloquent\Traits;
 
-use Illuminate\Contracts\Database\Query\Builder;
-
 /**
- * @mixin Builder
+ * @deprecated
  */
 trait HasDeletedAt
 {
-    /**
-     * @return static
-     */
-    public function onlyTrashed($boolean = 'and')
-    {
-        return $this->where($this->qualifyColumn('deleted_at'), '<=', now(), boolean: $boolean);
-    }
-
-    /**
-     * @return static
-     */
-    public function orOnlyTrashed()
-    {
-        return $this->onlyTrashed('or');
-    }
-
-    /**
-     * @return static
-     */
-    public function withoutTrashed($boolean = 'and')
-    {
-        return $this->where(fn(Builder $builder) => $builder
-            ->whereNull($this->qualifyColumn('deleted_at'))
-            ->orWhere($this->qualifyColumn('deleted_at'), '>', now()),
-            boolean: $boolean
-        );
-    }
-
-    /**
-     * @return static
-     */
-    public function orWithoutTrashed()
-    {
-        return $this->withoutTrashed('or');
-    }
+    use HasPassiveDeletedAt;
 }
---
+++ src/Database/Eloquent/Traits/HasPassiveDeletedAt.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Codewiser\Database\Eloquent\Traits;
+
+use Illuminate\Contracts\Database\Query\Builder;
+
+/**
+ * @mixin Builder
+ */
+trait HasPassiveDeletedAt
+{
+    /**
+     * @return static
+     */
+    public function onlyTrashed($boolean = 'and')
+    {
+        return $this->where($this->qualifyColumn('deleted_at'), '<=', now(), boolean: $boolean);
+    }
+
+    /**
+     * @return static
+     */
+    public function orOnlyTrashed()
+    {
+        return $this->onlyTrashed('or');
+    }
+
+    /**
+     * @return static
+     */
+    public function withoutTrashed($boolean = 'and')
+    {
+        return $this->where(fn(Builder $builder) => $builder
+            ->whereNull($this->qualifyColumn('deleted_at'))
+            ->orWhere($this->qualifyColumn('deleted_at'), '>', now()),
+            boolean: $boolean
+        );
+    }
+
+    /**
+     * @return static
+     */
+    public function orWithoutTrashed()
+    {
+        return $this->withoutTrashed('or');
+    }
+}
